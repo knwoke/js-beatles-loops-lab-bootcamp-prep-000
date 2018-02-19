398d9137025f51cce58d7f0f835d121633493cfe
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-02-19 18:05:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 
-function johnLennonFacts(array){
-  const array = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir bou an a boy scout', 'He hated the sound of his own voice']
-  var newArray = [];
-  var i = 0;
-  while (i < array.length){
-    newArray.push(array[i] + "!!!")
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
     i++;
   }
-  return newArray
+  return newFacts;
 }
