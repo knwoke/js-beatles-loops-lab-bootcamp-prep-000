fff39951da50f86d777de0b90bf7f534737b9254
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-02-19 15:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(array){
-  while (var i > 0){
+  while (array[i] > 0){
     array.push (array[i] + '!!!');
   }
   return array
