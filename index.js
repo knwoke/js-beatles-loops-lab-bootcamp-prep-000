afd8f8548246baeb474a11d5e3ffc5a80f7b612e
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-02-19 13:46:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+const johnLennonFacts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir bou an a boy scout', 'He hated the sound of his own voice']
+var musicians = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Star'];
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
+
+
+
+function johnLennonFacts(){
+
+}
